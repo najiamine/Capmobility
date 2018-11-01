80c98eff2b7773bf16ac376498d6a1a486f00972
Amine <aminenaji55@gmail.com>
2018-11-01 14:24:52 +0000

    first commit

---
+++ src/main/java/com/capmobility/Capmobility/CapmobilityApplication.java
@@ -0,0 +1,12 @@
+package com.capmobility.Capmobility;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapmobilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapmobilityApplication.class, args);
+	}
+}
